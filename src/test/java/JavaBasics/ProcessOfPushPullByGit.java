d48eb86ee5cab23d17d48f175f4857bf712ae597
natasha <natashabhandari1999@gmail.com>
2024-04-23 21:40:21 +0530

    process

---
+++ src/test/java/JavaBasics/ProcessOfPushPullByGit.java
@@ -0,0 +1,56 @@
+package JavaBasics;
+
+public class ProcessOfPushPullByGit {
+    public static void main(String[] args) {
+//Steps to Perfroms the push n pull code::
+
+        /*Right click on java class (if project created newly then need to perform the
+        operation git commands for the first time to add project in git*/
+        //Open github to create reposatory
+        //open Intell j IDE -> Click on java class -Open In->Terminal(alt+F12)->
+        //Add commands-> git init-> git status->git add .->git commit -m"Adding comment for project"
+        // ->git branch -M main->git remote add origin https://github.com/NatashaBhandari/Day3Practice.git
+        // ->git push -u origin main
+        // After performing this steps project will get linked to created reposatory in github
+        //open git hub click on resposatory->click on code option
+
+        //To give access of our codes to others->Click on Setting
+        //                                  ->Click on Collaborators->It will ask password of git
+        //                                  ->After clicking password click on Add people button green color button
+        //                                  ->Write username of people in search box and click select
+        //                                   collaborator above.
+        //                                   ->After adding we have see the people who have acess in manage acess list.
+
+        //Now To access the code of other if they have added us in their repo:
+        //      ->Click on code option which is in green color button
+        //      ->copy the given link and open intellj IDE
+        //      ->Click on option file->close project.
+        //      ->Welcome to IntellJ IDE window will open then there->Get from VCS option need to click
+        //      ->Copy the url and click on clone-> click on option login via github->it will ask for authoriation-> give it
+        //      ->Git hug page will open and we can see the all the code which we have added and also other people have access for it
+        //      ->After complition of cloning in intejjJ IDEA we can see the code of people have added.
+
+        //Now to push our code in the git hub to their new repo:
+        //  -> Create new class -> click on add to git
+        //  -> after creation of class new class will visible in green color means it is local copy
+        //   ->before pushing code need to pull code first so ->click on java pacakge-> Git-> pull-> it will ask one popup for main branch-> click oK
+        //  -> to add this new class in git-> right click -> Git->Add
+        //                                             ->Git-> Commit fil
+        //                                             ->Click on new class check box file-> click on commit and push->click on push
+        //                                             ->new class file has added to git.
+        //                                             ->Open git now you can see you new class
+
+        //How to add changes of java class file in git hub:
+        //          ->Right click on pacakge-> Clik on Git-> then first pull the code
+        //          ->Right click on new class-> Click on Git-> Add
+        //                                             ->Git-> Commit file
+        //                                             ->Click on new class check box file-> click on commit and push->click on push
+        //                                             ->Changes has been added to class file in git.
+        //                                             ->Open git now you can see the changes in class.
+
+        //How to check history in Git hub:
+        //->In git hub you can see the option commit after clicking there we can see the history .
+
+
+    }
+}
